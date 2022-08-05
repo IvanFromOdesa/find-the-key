9cd592bea276990a7af340121e9390c6e0c0dcec
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2022-08-05 19:15:02 +0300

    Update 1

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,9 +12,18 @@ public class GamePanel extends JPanel /*implements Runnable*/ {
     final int MAX_SCREEN_COLUMN = 16;
     final int MAX_SCREEN_ROW = 12;
     final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COLUMN;
-    final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_COLUMN;
+    final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;
+
+    // PLAYER'S LOCATION
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    // FPS
+    int FPS = 60;
 
     Thread gameThread;
+    KeyHandler keyH = new KeyHandler();
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
