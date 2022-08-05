bd5169d4ae04173eee383b9d9945215efa12bbf0
IvanFromOdesa <krylosov382@gmail.com>
2022-08-05 17:24:37 +0300

    First commit

---
+++ src/GamePanel.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.stream.Collectors;
+
+public class GamePanel extends JPanel /*implements Runnable*/ {
+
+    // SCREEN SETTINGS
+    final int ORIGINAL_TILE_SIZE = 16;
+    final int SCALE = 3;
+
+    final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
+    final int MAX_SCREEN_COLUMN = 16;
+    final int MAX_SCREEN_ROW = 12;
+    final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COLUMN;
+    final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_COLUMN;
+
+    Thread gameThread;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.setFocusable(true);
+    }
+
+    /*@Override
+    public void run() {
+
+    }*/
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setTitle("Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
