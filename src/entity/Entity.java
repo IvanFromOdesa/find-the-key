97ea3b5f41b43f05f84de5027bce78a91e1cd834
IvanFromOdesa <krylosov382@gmail.com>
2022-08-08 15:36:04 +0300

    Added 1st player sprite

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,7 +1,12 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 
     protected int x, y;
     protected int speed;
+
+    protected BufferedImage stand, up1, up2, down1, down2, left1, left2, right1, right2;
+    protected String direction;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 import static main.GamePanel.TILE_SIZE;
 
@@ -16,28 +19,48 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "up";
+    }
+
+    public void getPlayerImage() {
+        try {
+            stand = ImageIO.read(getClass().getResourceAsStream("/player/Vita.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
         if(keyH.upPressed) {
+            direction = "up";
             y -= speed;
         } else if(keyH.downPressed) {
+            direction = "down";
             y += speed;
         } else if(keyH.leftPressed) {
+            direction = "left";
             x -= speed;
         } else if(keyH.rightPressed) {
+            direction = "right";
             x += speed;
         }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, TILE_SIZE, TILE_SIZE);
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "up":
+                image = stand;
+        }
+
+        g2.drawImage(image, x, y, TILE_SIZE, TILE_SIZE, null);
     }
 }
