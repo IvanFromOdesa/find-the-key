a7835d229f897040998fd396eab01133d540d325
IvanFromOdesa <krylosov382@gmail.com>
2022-08-08 14:22:32 +0300

    Changes by Ivan

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,29 +1,28 @@
+package main;
+
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
-import java.util.stream.Collectors;
 
 public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
-    final int ORIGINAL_TILE_SIZE = 16;
-    final int SCALE = 3;
+    private static final int ORIGINAL_TILE_SIZE = 16;
+    private static final int SCALE = 3;
 
-    final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
-    final int MAX_SCREEN_COLUMN = 16;
-    final int MAX_SCREEN_ROW = 12;
-    final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COLUMN;
-    final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;
-
-    // PLAYER'S LOCATION
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
+    public static final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
+    private static final int MAX_SCREEN_COLUMN = 16;
+    private static final int MAX_SCREEN_ROW = 12;
+    private static final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COLUMN;
+    private static final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;
 
     // FPS
     int FPS = 60;
 
     Thread gameThread;
     KeyHandler keyH = new KeyHandler();
+    Player player = new Player(this, keyH);
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -68,24 +67,15 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-        if(keyH.upPressed) {
-            playerY -= playerSpeed;
-        } else if(keyH.downPressed) {
-            playerY += playerSpeed;
-        } else if(keyH.leftPressed) {
-            playerX -= playerSpeed;
-        } else if(keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
-//
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, TILE_SIZE, TILE_SIZE);
+        player.draw(g2);
         g2.dispose();
     }
 }
